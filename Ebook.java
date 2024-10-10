ceefda4f7e71319b5b24da35515c34b358b960fb
Clarice Christine <142429120+clarisv@users.noreply.github.com>
2024-10-10 19:49:42 -0300

    Ebook.java

---
+++ Ebook.java
@@ -0,0 +1,16 @@
+package com.mycompany.polimorfismo;
+
+    public class Ebook extends Livro {
+    private double tamanhoArquivo;
+
+    public Ebook(String titulo, String autor, double preco, double tamanhoArquivo) {
+        super(titulo, autor, preco);
+        this.tamanhoArquivo = tamanhoArquivo;
+    }
+
+    @Override
+    public void exibirDetalhes() {
+        super.exibirDetalhes();
+        System.out.println("Tamanho do arquivo: " + tamanhoArquivo + " MB");
+    }
+}
